4cb0dbb67c98e9998845d5df2ef5d2bf01b777e9
rouVling <youqianiqian@126.com>
2025-03-03 01:17:54 +0800

    新增小时分钟选择

--- src/renderer/main/component/datePicker.tsx
+++ src/renderer/main/component/datePicker.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Button, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, MenuList } from "@mui/material";
 
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { MultiSectionDigitalClock } from '@mui/x-date-pickers/MultiSectionDigitalClock';
+
 
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import NavigateNextRoundedIcon from '@mui/icons-material/NavigateNextRounded';
@@ -29,6 +31,7 @@ export default function DateInput({ date, setDate }: Props) {
   const [datePickerLevel, setDatePickerLevel] = React.useState(0)
   const [datePickerAnchor, setDatePickerAnchor] = React.useState<HTMLElement | null>(null);
   const [selectedDate, setSelectedDate] = React.useState<Date | null>(null);
+  const [selectedTime, setSelectedTime] = React.useState<Date | null>(null);
 
   return <>
     {date ?
@@ -71,7 +74,7 @@ export default function DateInput({ date, setDate }: Props) {
             // setDate(dayjs().endOf("week").endOf('hour').endOf('minute'));
             setDate(dayjs().add(7 - (((dayjs().day() + 6) % 7) + 1), "day").endOf('day').endOf('hour').endOf('minute'));
           }}>
-            <ListItemText primary="这周" secondary={dayjs().add(7-((dayjs().day() + 6) % 7 + 1), "day").format("MM-DD") + " 周" + "日一二三四五六".charAt(dayjs().add(7-((dayjs().day() + 6) % 7 + 1), "day").day())} />
+            <ListItemText primary="这周" secondary={dayjs().add(7 - ((dayjs().day() + 6) % 7 + 1), "day").format("MM-DD") + " 周" + "日一二三四五六".charAt(dayjs().add(7 - ((dayjs().day() + 6) % 7 + 1), "day").day())} />
           </MenuItem>
           <MenuItem onClick={() => { setDatePickerLevel(2); }}>
             <ListItemText primary="选择" />
@@ -90,10 +93,22 @@ export default function DateInput({ date, setDate }: Props) {
       {
         datePickerLevel === 2 &&
         <>
-          <DateCalendar defaultValue={dayjs()} onChange={(val) => setSelectedDate(val)}/>
+          <DateCalendar defaultValue={dayjs().endOf("hour").endOf("minute")} onChange={(val) => setSelectedDate(val)} />
           <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
-            {/* <Button onClick={() => { setDatePickerLevel(0); }} color="error">清除</Button> */}
-            <Button onClick={() => { setDatePickerLevel(0); setDate(selectedDate)}}>确定</Button>
+            <Button onClick={() => { setDatePickerLevel(3); }}>进一步</Button>
+            <Button onClick={() => { setDatePickerLevel(0); setDate(selectedDate) }}>确定</Button>
+          </div>
+        </>
+      }
+      {
+        datePickerLevel === 3 &&
+        <>
+          <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-end" }}>
+            <MultiSectionDigitalClock value={selectedTime ? dayjs(selectedTime) : dayjs()} onChange={(val) => { setSelectedTime(val.toDate()); }} />
+            <Button onClick={() => { setDatePickerLevel(2); }}>返回</Button>
+            <Button onClick={() => { setDatePickerLevel(0);
+              setDate(dayjs(selectedDate).set("hour", dayjs(selectedTime).hour()).set("minute", dayjs(selectedTime).minute()).toDate());
+             }}>确认</Button>
           </div>
         </>
       }
